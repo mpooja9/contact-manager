8703d3871b832804b8a7c7cd7f8ecc60e6945b1c
mpooja9 <160688974+mpooja9@users.noreply.github.com>
2024-02-21 13:34:49 +0530

    Setup Environment

---
+++ backend/Server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+
+// Set up MongoDB connection
+mongoose.connect('mongodb://0.0.0.0:27017/contact-manager-db', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define routes and middleware as needed
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      My contact app routes and logic.
+    </div>
+  );
+}
+
+export default App;
